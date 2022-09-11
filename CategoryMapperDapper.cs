bd70140759fcd15bf7c8941c594c76024de64f14
Benjamin Camacho <jahbenjah@hotmail.com>
2022-09-10 21:58:33 -0500

    Crud Dapper

---
+++ CategoryMapperDapper.cs
@@ -0,0 +1,53 @@
+﻿using Npgsql;
+using Dapper;
+namespace Pagila;
+
+public class CategoryMapperDapper
+{
+    private static readonly string _connectionString = "Host=localhost:49153;Username=postgres;Password=postgrespw;Database=pagila";
+
+    public static async Task<int> Create(Category category)
+    {
+        if (category.Id == 0)
+        {
+            await using var connection = new NpgsqlConnection(_connectionString);
+            string sql = @"INSERT INTO public.category(name, last_update) 
+                           VALUES (@Name, @LastUpdate) 
+                           RETURNING category_id;";
+            category.Id = (int)await connection.ExecuteScalarAsync(sql, new { Name = category.Name, LastUpdate = category.LastUpdated });
+        }
+        return category.Id;
+    }
+    public static async Task<Category> Read(int id)
+    {
+        await using NpgsqlConnection connection = new(_connectionString);
+        string sql = @"SELECT category_id,
+                              name, 
+                              last_update 
+                      FROM public.category 
+                      WHERE category_id = @Id 
+                      limit 1;";
+        return connection.QueryFirstOrDefault<Category>(sql, new { Id = id });
+    }
+    public static async Task<bool> Update(Category category)
+    {
+        int result = 0;
+        if (category.Id > 0)
+        {
+            await using NpgsqlConnection connection = new NpgsqlConnection(_connectionString);
+            string sql = @"UPDATE public.category 
+                          SET  name= @Name , 
+                               last_update= @LastUpdate
+                          WHERE category_id = @Id;";
+            result = await connection.ExecuteAsync(sql, new { Name = category.Name, LastUpdate = category.LastUpdated, Id = category.Id });
+        }
+        return result > 0;
+    }
+    public static async Task<bool> Delete(int id)
+    {
+        await using var connection = new NpgsqlConnection(_connectionString);
+        string sql = @"DELETE FROM public.category WHERE category_id = @Id;";
+        var result = await connection.ExecuteAsync(sql, new { Id = id });
+        return result > 0;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -2,18 +2,18 @@
 
 //Create
 var category = new Category { Name = "Benjamin Category 2022", LastUpdated = DateTime.Now };
-var categoryId = await CategoryMapper.Create(category);
+var categoryId = await CategoryMapperDapper.Create(category);
 
 //Read
-var cat = await CategoryMapper.Read(categoryId);
+var cat = await CategoryMapperDapper.Read(categoryId);
 Console.WriteLine($"{cat.Name} ,{cat.Id},{cat.LastUpdated} ");
 
-//Update
-cat.Name += "Actualizada";
-cat.LastUpdated = DateTime.Now;
-await CategoryMapper.Update(cat);
-var cat2 = await CategoryMapper.Read(cat.Id);
-Console.WriteLine($"{cat2.Name} {cat2.Id},{cat2.LastUpdated} ");
+////Update
+//cat.Name += "Actualizada";
+//cat.LastUpdated = DateTime.Now;
+//await CategoryMapper.Update(cat);
+//var cat2 = await CategoryMapper.Read(cat.Id);
+//Console.WriteLine($"{cat2.Name} {cat2.Id},{cat2.LastUpdated} ");
 
 //Delete Demo
 //await CategoryMapper.Delete(30);
